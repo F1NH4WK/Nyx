3b5e8729813e12585f32af4d521e1ac134c1012f
F1NH4WK <araujoplayer@gmail.com>
2022-09-17 13:43:22 -0300

    First Commit

---
+++ App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { MotiView } from 'moti';
+import Lottie from 'lottie-react-native'
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Lottie 
+      source={require('./src/animations/cart.json')}
+      autoPlay = {true}
+      loop = {true}/>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
